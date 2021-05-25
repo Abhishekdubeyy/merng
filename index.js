481487d8fe5236e82acce10e533c0db753ba5e78
Abhishek Dubey <32068667+Abhishekrkd@users.noreply.github.com>
2021-05-26 01:09:43 +0530

    main build

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const {ApolloServer} = require('apollo-server');
-
-const mongoose = require ('mongoose');
-
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
-const  resolvers = require('./graphql/resolvers/index.js');
-const { MONGODB } = require ('./config.js');   
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
 
+const pubsub = new PubSub();
 
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers       
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub })
 });
 
-
-//connecting to database /////////
 mongoose
-    .connect(MONGODB, { useNewUrlParser : true })  //otherwise will give deprecation warning if we dont
-    .then(() => {
-        console.log('Mongodb connected');  //to show db connected
-        return server.listen({ port : 5000 });
-    }) 
-    .then((res) => {                      //server.listen({port: 5000}) //server running
-        console.log(`Server running at ${res.url}`);
-    })
-    .catch(err => {
-        console.error(err)
-    })
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.error(err)
+  })
 
-    // opeining link transfer us to graphql page
+
+ 
